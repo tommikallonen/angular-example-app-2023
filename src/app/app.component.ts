051151e1e4322d9f4b0c4d93936d2231388a9b75
Tommi Kallonen <tommi.kallonen@lab.fi>
2023-11-09 13:37:35 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import { MatIconModule } from "@angular/material/icon";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [MatIconModule]
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-example-app';
