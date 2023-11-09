0c33a393544abf6bb3dbdb4dc526bab969701853
Tommi Kallonen <tommi.kallonen@lab.fi>
2023-11-09 15:12:31 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,11 +29,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-example-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Dev-ops-kurssin harjoitusprojekti');
-  });
 });
