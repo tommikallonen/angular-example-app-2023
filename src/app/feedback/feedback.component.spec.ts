59a08ab9383c68efba38acdfbc4c6f6b2500e8b3
Tommi Kallonen <tommi.kallonen@lab.fi>
2023-11-09 14:50:19 +0200

    Update feedback.component.spec.ts

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeedbackComponent } from './feedback.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+//import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
@@ -10,9 +10,9 @@ describe('FeedbackComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FeedbackComponent ],
-      schemas: [
-        CUSTOM_ELEMENTS_SCHEMA
-      ],
+      //schemas: [
+      //  CUSTOM_ELEMENTS_SCHEMA
+      //],
     })
     .compileComponents();
 
